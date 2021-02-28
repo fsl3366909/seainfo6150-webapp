e84bc8ed138f12d7715d878f2ef5499c7b0ee15d
Feng_Shaolong_001023876 <feng.shao@northeastern.edu>
2021-02-28 12:53:30 +0800

    [your commit message]

--- src/DynamicArticle/DynamicArticle.jsx
+++ src/DynamicArticle/DynamicArticle.jsx
@@ -5,8 +5,16 @@ const DynamicArticle = (props) => {
   return (
     <article>
       {/* add extra html with javascript attributes here */}
-      <HTMLText text={props.article.text} />
-    </article>
+          <header>
+              <h1>{props.article.title}</h1>
+              <p><i>by {props.article.author} (<a href={props.article.authorEmail}>{props.article.authorEmail}</a>)</i><br /></p>
+              <p>{props.article.displayDate} </p>
+              <p><br /> Published time stamp: {props.article.timeStamp}<br /></p>
+          </header>
+          <main>
+              <HTMLText text={props.article.text} />
+          </main>
+      </article>
   );
 };
 
